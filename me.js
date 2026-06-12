3878651f483f159531b41a5d282a38cd0634e956
tmonroe6-coder <tmonroe6@stumail.jccc.edu>
2026-06-12 13:38:56 -0500

    Add files via upload

---
+++ me.js
@@ -0,0 +1,47 @@
+// Variable : myName
+// Data type: String
+// Explanation: My name is Tierney 
+
+// Variable: favoriteColor
+// Data Type: String
+// Explanation: My favorite color is pink
+// Variable: myAge
+// Data Type: Number
+// Explanation: 21
+
+// Variable: favoriteFood
+// Data Type: String
+// Explanation: Pasta
+
+// Variable:favoriteanimal
+// Data Type: String
+// Explanation: Dogs and Cats
+
+// Variable:favoriteseason
+// Data Type: String
+// Explanation: Fall
+
+// Variable:IsStudent
+// Data Type: Boolean
+// Explanation: True
+
+let myName = "Tierney Monroe";
+let favoriteColor = "Pink";
+let myAge = 21;
+let favoriteFood = "Pasta";
+let favoriteAnimal = "Dogs and Cats";
+let favoriteSeason = "Fall";
+let isStudent = true;
+
+console.log(`My name is ${myName}. My favorite color is ${favoriteColor} and my favorite food is ${favoriteFood}.`);
+
+console.log(`My favorite season is ${favoriteSeason}, and I love ${favoriteAnimal}.`);
+
+// 1. Which data type was easiest to choose? Why?
+//String was the easiest one for me to understand because it represents textual data.
+// 2. Which variable was hardest to decide a data type for?
+//Boolean because I never have heard or seen this data type before until I looked it up. 
+// 3. What happens if a number is stored as a string in JavaScript?
+// JavaScript treats it as textual data rather than a mathematical entity
+// 4. Why is it helpful to plan data types before coding?
+// it is helpful to plan because it will prevent errors within the code. 
